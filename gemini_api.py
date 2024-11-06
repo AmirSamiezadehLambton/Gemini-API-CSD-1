250ca2463baf3dc3c44e3030c4340858b1a38e2a
Amir Samiezadeh <amir.s@orthobiomed.ca>
2024-11-06 18:07:39 -0500

    first version of fully working API

--- gemini_api.py
+++ gemini_api.py
@@ -5,7 +5,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+instructions = """
+You are the social media content creator. Your task is to promote our software solutions and generate
+social medial content for that. The social media channels are instagram and LinkedIn.
+You can only generate content for these platforms and only answer questions related to marketing and
+product promotion. Any other question besides this and greeting must be answered with
+'I can only answer questions about marketing and product promotion.'
+
+Write your answer in markdown, include details and reasoning.
+"""
+
 genai.configure(api_key=os.environ["API_KEY"])
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("What is DevOps and Cloud Computing?")
-print(response.text)
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash"
+    # system_instruction=instructions
+)
+
+
+def gemini_model(user_prompt):
+    response = model.generate_content(user_prompt, tools='google_search_retrieval')
+    return response.text
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask, request
+from gemini_api import gemini_model
 app = Flask(__name__)
 
 @app.route("/chat", methods=["POST"])
 def chat():
+    # Getting JSON data from POST request
     data = request.json
+
+    # Extracting the "prompt" from the JSON data
     prompt = data["prompt"]
-    return prompt
+
+    # Calling the function "gemini_model" in the gemini_api.py
+    output = gemini_model(user_prompt=prompt)
+
+    # Returning the model response as the output
+    return output
 
 
 if __name__ == "__main__":
